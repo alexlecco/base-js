24cd046e1cd23cdd2521335911530a831ac9196d
alexlecco <alexlecco24@gmail.com>
2019-12-02 10:41:52 -0300

    add local login workflow

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,33 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+window.onload = hideLogoutBtn;
+
+function hideLogoutBtn() {
+    document.getElementById("logoutBtn").hidden = true;
+}
+
+window.login = function() {
+    let user = document.getElementById("user").value;
+    let pass = document.getElementById("pass").value;
+    
+    localStorage.setItem("user", user);
+    localStorage.setItem("pass", pass);
+    
+    document.getElementById("user").value = '';
+    document.getElementById("pass").value = '';
+    
+    document.getElementById("loginBtn").hidden = true;
+    document.getElementById("logoutBtn").hidden = false;
+    document.getElementById("greetings").innerHTML = `<h1>Bienvenido ${user}</h1>`;
+};
+
+window.logout = function() {
+    localStorage.setItem("user", "");
+    localStorage.setItem("pass", "");
+
+    document.getElementById("greetings").innerHTML = `<h1></h1>`;
+    document.getElementById("logoutBtn").hidden = true;
+    document.getElementById("loginBtn").hidden = false;
+}
