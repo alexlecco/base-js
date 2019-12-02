85103ce9d9dd0b89c877d0f0f3644f58c8239fcb
alexlecco <alexlecco24@gmail.com>
2019-12-02 10:56:08 -0300

    add logged global variable

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,8 @@ console.log('webpack starterkit');
 
 window.onload = hideLogoutBtn;
 
+let logged = false;
+
 function hideLogoutBtn() {
     document.getElementById("logoutBtn").hidden = true;
 }
@@ -11,23 +13,35 @@ function hideLogoutBtn() {
 window.login = function() {
     let user = document.getElementById("user").value;
     let pass = document.getElementById("pass").value;
-    
-    localStorage.setItem("user", user);
-    localStorage.setItem("pass", pass);
-    
-    document.getElementById("user").value = '';
-    document.getElementById("pass").value = '';
-    
-    document.getElementById("loginBtn").hidden = true;
-    document.getElementById("logoutBtn").hidden = false;
-    document.getElementById("greetings").innerHTML = `<h1>Bienvenido ${user}</h1>`;
+
+    if(user && pass) {
+        logged = true;
+        
+        localStorage.setItem("user", user);
+        localStorage.setItem("pass", pass);
+        
+        document.getElementById("user").value = '';
+        document.getElementById("pass").value = '';
+        
+        document.getElementById("loginBtn").hidden = true;
+        document.getElementById("logoutBtn").hidden = false;
+        document.getElementById("greetings").innerHTML = `<h1>Bienvenido ${user}</h1>`;
+        
+        console.log("Logged User: ", logged)    
+    } else {
+        console.log("Debe ingresar usuario y contraseña")
+    }
 };
 
 window.logout = function() {
+    logged = false;
+
     localStorage.setItem("user", "");
     localStorage.setItem("pass", "");
 
     document.getElementById("greetings").innerHTML = `<h1></h1>`;
     document.getElementById("logoutBtn").hidden = true;
     document.getElementById("loginBtn").hidden = false;
+
+    console.log("Logged User: ", logged)
 }
