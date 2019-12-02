416fc8e5c6614e5d953fa10c20f6b5039e206ac4
alexlecco <alexlecco24@gmail.com>
2019-12-02 11:19:19 -0300

    add hard-coded users validation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,30 @@ import '../styles/index.scss';
 
 console.log('webpack starterkit');
 
+// when app starts
 window.onload = hideLogoutBtn;
 
+// variables
 let logged = false;
+let users = [
+    {
+        user: "alex",
+        pass: "lecco",
+        name: "Alex Villecco"
+    },
+    {
+        user: "nahuel",
+        pass: "jose",
+        name: "Nahuel Jose"
+    },
+    {
+        user: "juan",
+        pass: "usand",
+        name: "Juan Usandivaras"
+    },
+];
 
+// functions
 function hideLogoutBtn() {
     document.getElementById("logoutBtn").hidden = true;
 }
@@ -15,21 +35,32 @@ window.login = function() {
     let pass = document.getElementById("pass").value;
 
     if(user && pass) {
-        logged = true;
-        
-        localStorage.setItem("user", user);
-        localStorage.setItem("pass", pass);
-        
-        document.getElementById("user").value = '';
-        document.getElementById("pass").value = '';
-        
-        document.getElementById("loginBtn").hidden = true;
-        document.getElementById("logoutBtn").hidden = false;
-        document.getElementById("greetings").innerHTML = `<h1>Bienvenido ${user}</h1>`;
-        
-        console.log("Logged User: ", logged)    
+        for(var i = 0; i < users.length; i++) {
+            // check is user input matches username and password of a current index of the users array
+            if(user == users[i].user && pass == users[i].pass) {
+                
+                logged = true;
+            
+                localStorage.setItem("user", user);
+                localStorage.setItem("pass", pass);
+                
+                document.getElementById("user").value = '';
+                document.getElementById("pass").value = '';
+                
+                document.getElementById("loginBtn").hidden = true;
+                document.getElementById("logoutBtn").hidden = false;
+                document.getElementById("greetings").innerHTML = `<h1>Bienvenido ${users[i].name}</h1>`;
+                
+                console.log("Logged User: ", logged)
+
+                // stop the function if this is found to be true
+                return
+            }
+        }
+
+        if (!logged) alert("Usuario incorrecto")
     } else {
-        console.log("Debe ingresar usuario y contraseña")
+        alert("Debe ingresar usuario y contraseña")
     }
 };
 
